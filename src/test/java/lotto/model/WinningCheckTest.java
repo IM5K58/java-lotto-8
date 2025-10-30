8d8b1f52f5812984150540d177e6466538ac576e
IM5K58 <gitue11@gmail.com>
2025-10-30 23:17:25 +0900

    test(WinningCheckTest): WinningCheck 클래스에 대한 테스트 코드 작성

---
+++ src/test/java/lotto/model/WinningCheckTest.java
@@ -0,0 +1,33 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class WinningCheckTest {
+
+    @Test
+    @DisplayName("당첨된 경우를 확인하고, 총 당첨 금액을 계산한다.")
+    void 계산_테스트(){
+        Lottos lottos = new Lottos();
+        Lotto winningNumbers = new Lotto(List.of(1,2,3,4,5,6));
+        int bonusNumber = 7;
+        WinningCheck winningCheck = new WinningCheck(winningNumbers, bonusNumber); //당첨번호와 보너스번호를 파라미터로 받아 생성자 생성
+        lottos.addLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6))); //1등
+        lottos.addLotto(new Lotto(List.of(1, 2, 3, 4, 5, 7))); // 2등
+        lottos.addLotto(new Lotto(List.of(1, 2, 3, 4, 5, 8))); // 3등
+        lottos.addLotto(new Lotto(List.of(1, 2, 3, 4, 8, 10))); // 4등
+        lottos.addLotto(new Lotto(List.of(1, 2, 3, 14, 18, 10))); // 5등
+        lottos.addLotto(new Lotto(List.of(11, 12, 13, 14, 18, 10))); // Miss
+
+        WinningCheck winningCheck = new WinningCheck();
+        long totalPrize = winningCheck.calculateTotalPrize(lottos);
+
+        long predictedPrize = 2_000_000_000 + 30_000_000 + 1_500_000 + 50_000 + 5_000;
+        assertThat(totalPrize).isEqualTo(predictedPrize);
+
+    }
+}
