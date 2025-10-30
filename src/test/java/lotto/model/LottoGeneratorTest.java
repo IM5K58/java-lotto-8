9bb845c398a6ceef88e9e0a783de928f5262f063
IM5K58 <gitue11@gmail.com>
2025-10-30 15:32:27 +0900

    test(LottoGeneratorTest): 로또 번호 랜덤 생성이 잘 되는지 테스트

--- src/test/java/lotto/model/LottoGeneratorTest.java
+++ src/test/java/lotto/model/LottoGeneratorTest.java
@@ -9,5 +9,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LottoGeneratorTest {
 
+    @Test
+    @DisplayName("지정한 범위 내에서 지정한 개수 만큼의 정수를 List로 반환한다.")
+    void 로또_번호_생성_테스트(){
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<Integer> res = lottoGenerator.generateLottoNumber();
+        System.out.println(res);
+    }   
 
 }
