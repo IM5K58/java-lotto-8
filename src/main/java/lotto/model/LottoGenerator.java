14390e1381baec0c047cc77cb896f15e25898e8a
IM5K58 <gitue11@gmail.com>
2025-10-30 13:14:09 +0900

    chore: 패키지 내 클래스 생성

---
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -0,0 +1,14 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoGenerator {
+
+    public List<Integer> generateLottoNumber;
+
+    public Integer generateBonusNumber;
+    
+
+}
