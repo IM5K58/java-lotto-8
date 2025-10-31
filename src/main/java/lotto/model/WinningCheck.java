e19238ee778224d2eb0b63009e3067f933ec1132
gitue <gitue11@gmail.com>
2025-10-31 11:39:41 +0900

    feat(WinningCheck): 당첨 횟수 판별 기능 추가

--- src/main/java/lotto/model/WinningCheck.java
+++ src/main/java/lotto/model/WinningCheck.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import java.util.Map;
+import java.util.EnumMap;
 
 public class WinningCheck {
     Lotto winningNumber;
@@ -11,22 +13,34 @@ public class WinningCheck {
         this.bonusNumber = number;
     }
 
-    public long calculateTotalPrize(Lottos lottos){
+    public long calculateTotalPrize(Map<WinningRank, Integer> winningTimes){
         long totalPrize = 0L;
-        for(Lotto lotto : lottos.getLottos()){
-            int matchCount = lotto.matchCount(this.winningNumber);
-            boolean bonusMatch = lotto.matchBonus(this.bonusNumber);
+        for(Map.Entry<WinningRank, Integer> entry : winningTimes.entrySet()){
+            WinningRank winningRank = entry.getKey();
+            int count = entry.getValue();
 
-            WinningRank rank = WinningRank.findRank(matchCount, bonusMatch);
-            totalPrize += rank.getPrizeMoney();
+            totalPrize += (long)winningRank.getPrizeMoney() * count;
         }
         return totalPrize;
     }
 
-    private boolean bonusCheck(int num){
-        if(num != bonusNumber){
-            return false;
+    public Map<WinningRank, Integer> getWinningTimes(Lottos lottos) {
+        Map<WinningRank, Integer> winningTimes = new EnumMap<>(WinningRank.class);
+
+        countWinnings(lottos, winningTimes);
+        return winningTimes;
+    }
+
+    private void countWinnings(Lottos lottos, Map<WinningRank, Integer> winningTimes) {
+        for (WinningRank rank : WinningRank.values()) {
+            winningTimes.put(rank, 0);
         }
-        return true;
+        for (Lotto lotto : lottos.getLottos()) {
+            int matchCount = lotto.matchCount(this.winningNumber);
+            boolean bonusMatch = lotto.matchBonus(this.bonusNumber);
+            WinningRank rank = WinningRank.findRank(matchCount, bonusMatch); //
+
+            winningTimes.put(rank, winningTimes.get(rank) + 1);
+        }
     }
 }
--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -1,4 +1,4 @@
-package lotto.model;
+package lotto.utils;
 
 public class Validator {
 }
