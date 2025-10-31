3b1663f9001146227b11ac58566c11ef3b4eac92
gitue <gitue11@gmail.com>
2025-10-31 11:58:19 +0900

    feat(Output): Output 기능 구현

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,5 +1,18 @@
 package lotto.view;
 
 public class Output {
+    public void showLottoCounts(int lottocount){
+        System.out.println(lottocount+"개를 구매했습니다.");
+    }
 
+    public void showLottos(){
+
+    }
+
+    public void showWinningCounts(){
+
+    }
+    public void showROI(double roi){
+        System.out.print("총 수익률은 "+roi+"%입니다.");
+    }
 }
