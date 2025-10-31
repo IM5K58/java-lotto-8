5624469d12ed10e7fef00fedbaad416e31fe3e44
gitue <gitue11@gmail.com>
2025-10-31 11:43:41 +0900

    test(WinningCheckTest): 테스트 코드 수정

--- src/test/java/lotto/model/WinningCheckTest.java
+++ src/test/java/lotto/model/WinningCheckTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class WinningCheckTest {
 
     @Test
-    @DisplayName("당첨된 경우를 확인하고, 총 당첨 금액을 계산한다.")
+    @DisplayName("당첨된 경우를 map에 저장하고, 총 당첨 금액을 계산한다.")
     void 계산_테스트(){
         Lottos lottos = new Lottos();
         Lotto winningNumbers = new Lotto(List.of(1,2,3,4,5,6));
@@ -22,7 +23,9 @@ class WinningCheckTest {
         lottos.addLotto(new Lotto(List.of(1, 2, 3, 4, 8, 10))); // 4등
         lottos.addLotto(new Lotto(List.of(1, 2, 3, 14, 18, 10))); // 5등
         lottos.addLotto(new Lotto(List.of(11, 12, 13, 14, 18, 10))); // Miss
-        long totalPrize = winningCheck.calculateTotalPrize(lottos);
+
+        Map<WinningRank,Integer>winnings = winningCheck.getWinningTimes(lottos);
+        long totalPrize = winningCheck.calculateTotalPrize(winnings);
 
         long predictedPrize = 2_000_000_000 + 30_000_000 + 1_500_000 + 50_000 + 5_000;
         assertThat(totalPrize).isEqualTo(predictedPrize);
