3cdf54fa58db684791a28d8f2b82f0ad56186f4c
gitue <gitue11@gmail.com>
2025-11-02 01:21:28 +0900

    feat(LottosDTO): LottosDTO 기능구현

--- src/main/java/lotto/dto/LottoDTO.java
+++ src/main/java/lotto/dto/LottoDTO.java
@@ -6,12 +6,11 @@ import java.util.List;
 
 public class LottoDTO {
 
-    Lotto lottoDTO;
+    private final List<Integer> lottoDTO;
     LottoDTO(List<Integer> lotto) {
-        this.lottoDTO = new Lotto(lotto);
+        this.lottoDTO = lotto;
     }
-    //Lottos에서 List Numbers를 LottoDTO로 변경하여 넘겨주도록 설정
-    public Lotto getLottoDTO() {
+    public List<Integer> getLottoDTO() {
         return this.lottoDTO;
     }
 }
--- src/main/java/lotto/dto/LottosDTO.java
+++ src/main/java/lotto/dto/LottosDTO.java
@@ -1,1 +1,15 @@
-//LottoDTO를 파라미터로 받는 새성자
+package lotto.dto;
+
+import java.util.List;
+
+public class LottosDTO {
+    private final List<LottoDTO> lottos;
+
+    public LottosDTO(List<LottoDTO> lottos) {
+        this.lottos = lottos;
+    }
+
+    public List<LottoDTO> getLottos() {
+        return lottos;
+    }
+}
